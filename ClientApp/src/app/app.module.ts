a2bc21f1f1fad11493fcffdcbe3b11bd8ece990e
Äkwav <ekwav@coflnet.com>
2023-02-28 13:53:25 +0100

    continue

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -8,21 +8,27 @@ import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
+import { ApiModule } from './api.module';
+import { EditorComponent } from './editor/editor.component';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent
+    EditorComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    NgSelectModule,
+    ApiModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
+      { path: 'editor', component: EditorComponent}
     ])
   ],
   providers: [],
