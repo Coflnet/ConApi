3f9ee68fa194346762e4e390da755d5a246d5278
Äkwav <ekwav@coflnet.com>
2024-12-24 18:06:42 +0100

    Add firebase auth fix #4

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
-using System.Reflection;
 using System.Text.Json.Serialization;
 using Coflnet.Auth;
 using Coflnet.Connections.Services;
 using Coflnet.Core;
-using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.SwaggerGen;
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,47 +12,7 @@ builder.Services.AddSingleton<PersonService>();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "ConApi",
-                    Version = "v1",
-                    Description = ""
-                });
-                // baarer token 
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Bearer {token}\"",
-                    In = ParameterLocation.Header,
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer"
-                });
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        new string[] { }
-                    }
-                });
-                c.CustomOperationIds(apiDesc =>
-                {
-                    return (apiDesc.TryGetMethodInfo(out MethodInfo methodInfo) ? methodInfo.Name : "xy") + (apiDesc.HttpMethod == "OPTIONS" ? "Options" : "");
-                });
 
-                // Set the comments path for the Swagger JSON and UI.
-                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                c.IncludeXmlComments(xmlPath, true);
-            });
 builder.Services.AddControllers().AddJsonOptions(o =>
 {
     o.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
@@ -72,17 +31,6 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-app.UseSwagger(c =>
-{
-    c.RouteTemplate = "api/openapi/{documentName}/openapi.json";
-})
-.UseSwaggerUI(c =>
-{
-    c.RoutePrefix = "api";
-    c.SwaggerEndpoint("/api/openapi/v1/openapi.json", "Con");
-    c.EnablePersistAuthorization();
-});
-
 app.UseRouting();
 app.UseCoflnetCore();
 app.UseCors("AllowAll");
@@ -94,8 +42,19 @@ app.Use(async (context, next) =>
     var responseCode = context.Response.StatusCode;
     Console.WriteLine($"Response: {responseCode}");
 });
-app.UseAuthentication();
-app.UseAuthorization();
+app.UseCoflAuthService();
 app.MapControllers();
+
+var googleConfigpath = app.Configuration["GOOGLE_APPLICATION_CREDENTIALS"];
+if (googleConfigpath == null)
+    app.Logger.LogWarning("GOOGLE_APPLICATION_CREDENTIALS not set, not initializing Firebase");
+else
+{
+    var credentials = await  GoogleCredential.FromFileAsync(googleConfigpath, default);
+    FirebaseApp.Create(new AppOptions()
+    {
+        Credential = credentials
+    });
+}
 
 app.Run();
